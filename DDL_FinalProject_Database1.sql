80f5dad92b4d91d1d9c25f72b6152a297eff1270
Diego F Rueda Baez <75450503+DiegoRuedaBaez-dev@users.noreply.github.com>
2024-05-14 06:32:28 -0500

    Update DDL_FinalProject_Database1.sql

--- DDL_FinalProject_Database1.sql
+++ DDL_FinalProject_Database1.sql
@@ -1,29 +1,27 @@
 CREATE TABLE country(
-    id_country INT(11) NOT NULL,
+    id_country INT(11) NOT NULL AUTO_INCREMENT,
     vname VARCHAR(100) NOT NULL,
     PRIMARY KEY(id_country)
 );
 
 CREATE TABLE region(
-    id_region INT(11) NOT NULL,
+    id_region INT(11) NOT NULL AUTO_INCREMENT,
     vname VARCHAR(100) NOT NULL,
     id_country INT(11) NOT NULL,
     PRIMARY KEY(id_region),
-    CONSTRAINT FK_region_country FOREIGN KEY (id_country) REFERENCES 
-    country(id_country)
+    CONSTRAINT FK_region_country FOREIGN KEY (id_country) REFERENCES country(id_country)
 );
 
 CREATE TABLE city(
-    id_city INT(11) NOT NULL,
+    id_city INT(11) NOT NULL AUTO_INCREMENT,
     vname VARCHAR(100) NOT NULL,
     id_region INT(11) NOT NULL,
     PRIMARY KEY(id_city),
-    CONSTRAINT FK_city_region FOREIGN KEY (id_region) REFERENCES 
-    region(id_region)
+    CONSTRAINT FK_city_region FOREIGN KEY (id_region) REFERENCES region(id_region)
 );
 
 CREATE TABLE vaddress(
-    id_vaddress INT(11) NOT NULL,
+    id_vaddress INT(11) NOT NULL AUTO_INCREMENT,
     linea_vaddress VARCHAR(50) NOT NULL,
     barrio VARCHAR(100) NOT NULL,
     codigo_postal VARCHAR(10),
@@ -33,7 +31,7 @@ CREATE TABLE vaddress(
 );
 
 CREATE TABLE vprovider(
-    id_vprovider INT(11) NOT NULL,
+    id_vprovider INT(11) NOT NULL AUTO_INCREMENT,
     phone VARCHAR(20) NOT NULL,
     vname VARCHAR(100) NOT NULL,
     PRIMARY KEY (id_vprovider)
@@ -48,7 +46,7 @@ CREATE TABLE vprovider_address (
 );
 
 CREATE TABLE office (
-    id_office INT(11) NOT NULL,
+    id_office INT(11) NOT NULL AUTO_INCREMENT,
     vname VARCHAR(100) NOT NULL,
     phone VARCHAR(20),
     PRIMARY KEY (id_office)
@@ -63,7 +61,7 @@ CREATE TABLE office_vaddress (
 );
 
 CREATE TABLE product_brand(
-    id_brand INT(11) NOT NULL,
+    id_brand INT(11) NOT NULL AUTO_INCREMENT,
     brand VARCHAR (50) NOT NULL,
     descripcion_texto TEXT,
     descripcion_html TEXT,
@@ -72,18 +70,17 @@ CREATE TABLE product_brand(
 );
 
 CREATE TABLE measurements(
-    id_measurements INT(11) NOT NULL,
-    alto DECIMAL(5),
-    ancho DECIMAL(5),
-    profundidad INT(5),
-    diametro DECIMAL(5),
+    id_measurements INT(11) NOT NULL AUTO_INCREMENT,
+    alto DECIMAL(5,2),
+    ancho DECIMAL(5,2),
+    profundidad DECIMAL(5,2),
+    diametro DECIMAL(5,2),
     unidad_de_medida VARCHAR(10),
     PRIMARY KEY (id_measurements)
 );
 
 CREATE TABLE products(
-    id_products INT(11) NOT NULL,
-    PRIMARY KEY (id_products),
+    id_products INT(11) NOT NULL AUTO_INCREMENT,
     vname VARCHAR(100) NOT NULL,
     descrip TEXT,
     provider_price DECIMAL (12,5),
@@ -93,11 +90,12 @@ CREATE TABLE products(
     id_vprovider INT(11),
     FOREIGN KEY (id_vprovider) REFERENCES vprovider(id_vprovider),
     id_brand INT(11) NOT NULL,
-    FOREIGN KEY (id_brand) REFERENCES product_brand(id_brand)
+    FOREIGN KEY (id_brand) REFERENCES product_brand(id_brand),
+    PRIMARY KEY (id_products)
 );
 
 CREATE TABLE inventory (
-    id_inventory INT(11) NOT NULL,
+    id_inventory INT(11) NOT NULL AUTO_INCREMENT,
     id_products INT(11) NOT NULL,
     cantidad_en_stock SMALLINT(6) NOT NULL,
     ubicacion VARCHAR(100), 
@@ -106,7 +104,7 @@ CREATE TABLE inventory (
 );
 
 CREATE TABLE employees (
-    id_employees INT(11) NOT NULL,
+    id_employees INT(11) NOT NULL AUTO_INCREMENT,
     vname VARCHAR(50) NOT NULL,
     sn1 VARCHAR(50) NOT NULL,
     sn2 VARCHAR(50) NOT NULL,
@@ -116,21 +114,19 @@ CREATE TABLE employees (
     id_boss INT(11),
     position VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_employees),
-    CONSTRAINT FK_employees_office FOREIGN KEY (id_office) REFERENCES 	 
-    office(id_office),
-    CONSTRAINT FK_employees_boss FOREIGN KEY (id_boss) REFERENCES 
-    employees(id_employees)
+    CONSTRAINT FK_employees_office FOREIGN KEY (id_office) REFERENCES office(id_office),
+    CONSTRAINT FK_employees_boss FOREIGN KEY (id_boss) REFERENCES employees(id_employees)
 );
 
 CREATE TABLE contact_info (
-    id_contact_info INT(11) NOT NULL,
+    id_contact_info INT(11) NOT NULL AUTO_INCREMENT,
     vname_contact_info VARCHAR(50) NOT NULL,
     sn_contact_info VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_contact_info)
 );
 
 CREATE TABLE client (
-    id_client INT(11) NOT NULL,
+    id_client INT(11) NOT NULL AUTO_INCREMENT,
     vname_client VARCHAR(50) NOT NULL,
     sn1_client VARCHAR(50) NOT NULL,
     sn2_client VARCHAR(50) NOT NULL,
@@ -139,10 +135,8 @@ CREATE TABLE client (
     credit_limit DECIMAL(15,2),
     id_contact_info INT(11),
     PRIMARY KEY (id_client),
-    CONSTRAINT FK_contact_info_client FOREIGN KEY (id_contact_info) REFERENCES 
-    contact_info(id_contact_info),
-    CONSTRAINT FK_client_employees_sellrep FOREIGN KEY 
-    (id_employees_sellrep) REFERENCES employees(id_employees)
+    CONSTRAINT FK_contact_info_client FOREIGN KEY (id_contact_info) REFERENCES contact_info(id_contact_info),
+    CONSTRAINT FK_client_employees_sellrep FOREIGN KEY (id_employees_sellrep) REFERENCES employees(id_employees)
 );
 
 CREATE TABLE client_vaddress (
@@ -154,13 +148,13 @@ CREATE TABLE client_vaddress (
 );
 
 CREATE TABLE order_status(
-    id_status INT(11)  NOT NULL,
+    id_status INT(11) NOT NULL AUTO_INCREMENT,
     vstatus VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_status)
 );
 
 CREATE TABLE vorder(
-    id_vorder INT(11) NOT NULL,
+    id_vorder INT(11) NOT NULL AUTO_INCREMENT,
     date_order DATE NOT NULL,
     date_aprox DATE NOT NULL,
     date_arrive DATE,
@@ -184,11 +178,11 @@ CREATE TABLE order_details (
 );
 
 CREATE TABLE payment(
-id_transaction INT(100) NOT NULL,
-PRIMARY KEY (id_transaction),
-id_client INT(11) NOT NULL,
-payment_type VARCHAR(40) NOT NULL,
-date_payment DATE NOT NULL,
-total DECIMAL(15) NOT NULL,
-CONSTRAINT FK_payment_client FOREIGN KEY (id_client) REFERENCES client(id_client)
+    id_transaction INT(11) NOT NULL AUTO_INCREMENT,
+    id_client INT(11) NOT NULL,
+    payment_type VARCHAR(40) NOT NULL,
+    date_payment DATE NOT NULL,
+    total DECIMAL(15,2) NOT NULL,
+    PRIMARY KEY (id_transaction),
+    CONSTRAINT FK_payment_client FOREIGN KEY (id_client) REFERENCES client(id_client)
 );
